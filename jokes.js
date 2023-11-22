135997e16aed1097956d70b7117f87104efd93a4
MuhammadBeyyy <146830245+MuhammadBeyyy@users.noreply.github.com>
2023-11-22 04:40:20 -0800

    Update jokes.js

--- jokes.js
+++ jokes.js
@@ -1,1 +1,21 @@
+//fetch API
+const jokeContainer = document.getElementById('joke')
+const button = document.getElementById('btn')
+const url='https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single'
+
+
+let getJoke = () => {
+    fetch(url)
+    .then(response => response.json())
+    .then(item => {
+        jokeContainer.textContent = `${item.joke}`
+    })
+    .catch((err) => {
+        console.warn(err.message)
+    })
+}
+
+button.addEventListener('click',getJoke)
+getJoke()
+
 
